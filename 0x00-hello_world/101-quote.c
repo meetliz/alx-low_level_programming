8801746924509823bc9599d47b34df3e1a5e1283
meetliz <ifeoma.e.okonji@gmail.com>
2022-10-14 17:12:29 -0700

    The fourth file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /*
- * main - Entry
- * Return: 1
+ * main - Main Function to write a code
+ * Return: Always 1 (Success)
  */
+
 int main(void)
 {
-	write(1, “and that piece of art is useful\” -Dora Korpar, 2015-10-19\n”, 59);
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, q, 59);
 	return (1);
 }
