76de05bd7294ea2055ad67f6e684f22e95a2f05f
meetliz <ifeoma.e.okonji@gmail.com>
2022-10-17 16:07:03 -0700

    The combs

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,10 +11,8 @@ int main(void)
 
 	for (i = 48; i < 58; i++)
 	{
-		
 		for (j = i + 1; j < 58; j++)
 		{
-			
 			putchar(i);
 			putchar(j);
 			if (i != 56 && j != 58)
@@ -23,9 +21,7 @@ int main(void)
 				putchar(32);
 			}
 		}
-	
 	}
-
 	putchar(10);
 
 	return (0);
