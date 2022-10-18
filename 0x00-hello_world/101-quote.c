b9eac9f30c946d561dc445667918e3fe1fc20f1d
meetliz <ifeoma.e.okonji@gmail.com>
2022-10-17 18:23:01 -0700

    Okay

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*
- * main - Main Function to write a code
- * Return: Always 1 (Success)
+/**
+ * main - prints exactly and that piece of art is useful
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, q, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
