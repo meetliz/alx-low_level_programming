47ea29cee07eca6e91434c8c578debe22f7a6bbd
meetliz <ifeoma.e.okonji@gmail.com>
2022-10-18 13:22:25 -0700

    bet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,7 +7,7 @@ void print_alphabet(void)
 {
 	char lowercase;
 
-	for (lowercase = 'a'; lowercase <= 'z'; la++)
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 		_putchar(lowercase);
 
 	_putchar('\n');
