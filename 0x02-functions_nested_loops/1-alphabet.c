3bd60060c11ae13d8e5f82c06114a0e5d41da8a0
meetliz <ifeoma.e.okonji@gmail.com>
2022-10-18 13:18:39 -0700

    bet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  prints the alphabet, in lowercase
+ */
+void print_alphabet(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; la++)
+		_putchar(lowercase);
+
+	_putchar('\n');
+}
