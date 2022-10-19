ef056a8a68002b0e6ec2f5186a35126b8902aebe
meetliz <ifeoma.e.okonji@gmail.com>
2022-10-18 22:00:09 -0700

    jack

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
- * Return: Always 0
+ * jack_bauer - prints every minute of the day
  */
 void jack_bauer(void)
 {
@@ -17,7 +15,7 @@ void jack_bauer(void)
 			_putchar((hour % 10) + '0');
 			_putchar(':');
 			_putchar((minute / 10) + '0');
-			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
 			_putchar('\n');
 		}
 	}
