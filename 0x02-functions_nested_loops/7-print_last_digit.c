56f832c76b47d8cb85cad040bb9a65b8c6770554
meetliz <ifeoma.e.okonji@gmail.com>
2022-10-18 21:46:02 -0700

    its

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,11 @@
  */
 int print_last_digit(int n)
 {
-	int ltd = n % 10;
+	int ld = n % 10;
 
-	if (ltd < 0)
-		ltd *= -1;
-
-	_putchar(ltd + '0');
+	if (ld < 0)
+		ld *= -1;
+	_putchar(ld + '0');
 
 	return (0);
 }
