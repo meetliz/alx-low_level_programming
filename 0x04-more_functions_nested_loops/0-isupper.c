b588ab958e14d8a687676bff512dad82602a07dc
meetliz <ifeoma.e.okonji@gmail.com>
2022-10-20 18:50:54 -0700

    is

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,14 @@
 
 /**
  * _isupper - checks for uppercase character
- * @c: Variant text
+ * @c: carrier integer variable
  * Return: Always (0)
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
+	{
 		return (1);
-	else
-		return (0);
+	}
+	return (0);
 }
