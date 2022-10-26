800153a688d99f4d473878769e5b10847290ca08
meetliz <ifeoma.e.okonji@gmail.com>
2022-10-25 18:04:26 -0700

    lo

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,12 +2,12 @@
 
 /**
  * puts_half - prints half of a string, followed by a new line.
- * @str:half string to be printed
+ * @str: half string to be printed
  * Return: void
  */
 void puts_half(char *str)
 {
-	int len = 0
+	int len = 0;
 
 	while (*str != '\0')
 	{
