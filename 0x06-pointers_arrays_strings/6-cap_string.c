de7c1a61ab94a72bd4207d9868867df8a18b3a59
meetliz <ifeoma.e.okonji@gmail.com>
2022-10-27 04:50:33 -0700

    h

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,37 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * cap_string - capitalizes all words of a string
- * @s: pointer to char params
- * Return: *s
+ * @s: the string to be capitalized
+ * Return: A pointer to the changed string
  */
-char *cap_string(char *)
+char *cap_string(char *s)
 {
-	int i, j;
-	char delimeters[] = " \t\n,;.!?\"(){}";
+	int index = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (str[index])
 	{
-		if (s[0] >= 97 && s[0] <= 122)
-			s[0] = s[0] - 32;
-				for (j = 0; delimeters[j] != '\0'; j++)
-					if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
-						s[i + 1] = s[i + 1] - 32;
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+
+			index++;
+		if (str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}' ||
+				index == 0)
+			str[index] -= 32;
+
+		index++;
 	}
-	return (s);
+
+	return (str);
 }
