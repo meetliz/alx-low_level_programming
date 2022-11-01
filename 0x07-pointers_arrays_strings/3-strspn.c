ab10bff4511d92f464779e054d35ddef83391983
meetliz <ifeoma.e.okonji@gmail.com>
2022-10-31 18:02:15 -0700

    joha

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,9 +11,14 @@ unsigned int _strspn(char *s, char *accept)
 	i = l = 0;
 	while (s[i] != 0)
 	{
-		if (s[i] != accept[j])
+		j = 0;
+		a = 0;
+		while (accept[j] != 0)
 		{
-			j++;
+			if (s[i] != accept[j])
+			{
+				j++;
+			}
 			else if (s[i] == accept[j])
 			{
 				a++;
